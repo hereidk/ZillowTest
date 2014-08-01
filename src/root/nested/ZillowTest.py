e7018968eede5db44c046392f51668dcf6951288
hereidk <hereidk@gmail.com>
2014-08-01 10:58:59 -0400

    Pull agg address functions into AddressList class

--- src/root/nested/ZillowTest.py
+++ src/root/nested/ZillowTest.py
@@ -4,7 +4,7 @@ Created on Jul 1, 2014
 @author: kahere
 '''
 
-from root.nested.ZillowZip import ZipData, AddressData
+from root.nested.ZillowZip import ZipData, AddressData, AddressList
 import pandas
 import numpy as np
 import tkinter
@@ -36,115 +36,11 @@ def zips():
                 
     print (home_values)
     
-def address(address, test_zipcode):
-    # Call AddressData class methods
-    address_data = AddressData(address, test_zipcode)
-    results = address_data.get_deep_search_results()
-    
-    # Select desired property attributes from Zillow
-    attributes = ['result.address.street.contents',
-                  'result.address.city.contents',
-                  'result.address.state.contents',
-                  'result.address.zipcode.contents',
-                  'result.latitude.contents',
-                  'result.longitude.contents',
-                  'result.useCode.contents',
-                  'result.taxAssessment.contents',
-                  'result.yearBuilt.contents',
-                  'result.lotSizeSqFt.contents',
-                  'result.finishedSqFt.contents',
-                  'result.bedrooms.contents',
-                  'result.bathrooms.contents',
-                  'result.zestimate.amount.contents']
-    
-    def splitAttr(attrstring):
-        # Split attribute list, iteratively navigate XML tree
-        attrs = attrstring.split('.')
-        for i in range(len(attrs)):
-            if i == 0:
-                lvl_1 = getattr(results, attrs[i])
-            else:
-                lvl_2 = getattr(lvl_1, attrs[i]) 
-                lvl_1 = lvl_2
-                if i == len(attrs) - 1:
-                    lvl_1 = lvl_1[0]
-        return lvl_1        
-    
-    if results is not None:
-        dataset = [0] * len(attributes)
-        count = -1
-        
-        # Find results for all attributes, add to dataset list
-        for item in attributes:
-            count += 1
-            try:
-                dataset[count] = splitAttr(item)
-            except (AttributeError, IndexError):
-                continue            
-            
-        # Convert dataset to array, reshape output
-        dataset = np.array(dataset)
-        dataset = dataset.reshape((1,np.shape(dataset)[0]))
-        
-        return dataset
-    
-    else:
-        return
-
-def selectFile():
-    # Browse to directory of portfolio file
-    root2 = tkinter.Tk()
-    root2.withdraw()
-    currdir = os.getcwd()
-    validfile = False
-    while validfile == False: 
-        tempdir = tkFileDialog.askopenfilename(parent=root2, initialdir=currdir, title='Please select an address .csv file.')
-
-        # Make sure file is correct type
-        if tempdir.endswith('.csv'):
-            portfolio_file = tempdir
-            address_list = np.loadtxt(portfolio_file,delimiter=',',skiprows=1,dtype=str)
-            
-            # Make sure file is in correct format with expected number of columns
-            if not address_list.shape[1] == 4:
-                print ('Text file should have 4 columns: Street address, city, state, postal code. Please try again.')
-            else:
-                validfile = True 
-        
-        # Handle cancel
-        elif len(tempdir) == 0:
-            validfile = True
-            sys.exit()
-        
-        # If file exists but doesn't end with .csv, is wrong file type
-        else:
-            print("Error: File type must be .csv.")      
-    return address_list         
-
-def runAddress():    
-    # Attributes to collect from Zillow
-    column_list = columns=['street_address','city','state','zipcode','latitude','longitude','property_type','tax_assessment','year_built','lot_size','sq_ft','bedrooms','bathrooms','estimated_mkt_value']
-    address_list = selectFile()
-    
-    # Check each record to see if Zillow can find it, add results to address_info dataframe
-    address_info = pandas.DataFrame(columns=column_list)
-    for row in address_list:
-        single_address = row[0][2:-1]+' '+row[1][2:-1]+' '+row[2][2:-1]
-        zipcode = row[-1][2:-1]
-        
-        # Excel removes leading 0, this adds them back
-        if len(str(zipcode)) < 5:
-            zipcode = str(zipcode).zfill(5)    
-        
-        get_address = pandas.DataFrame(address(single_address,zipcode),columns=column_list)
-        address_info = address_info.append(get_address)
-
-    # Output address dataframe to .csv file
-    address_info.to_csv('address_test.csv',sep=',',index=False)
 
 if __name__ == '__main__':    
     
 #     zips()
 
-    runAddress()
+    address_list = AddressList()
+    address_list.runAddress()
         
--- src/root/nested/ZillowZip.py
+++ src/root/nested/ZillowZip.py
@@ -13,6 +13,10 @@ from requests.exceptions import (ConnectionError, TooManyRedirects,
 from bs4 import BeautifulSoup
 import numpy as np
 import sys
+import pandas
+import tkinter
+import tkinter.filedialog as tkFileDialog
+import os
 
 
 class ZipData():
@@ -122,4 +126,119 @@ class AddressData():
             print ('Error message: ', response.find_all('message/code')[0].text) 
         else:
             return response
+    
+class AddressList():
+    def __init__(self):
+        pass
+    
+    def runAddress(self):    
+        # Attributes to collect from Zillow
+        column_list = ['street_address','city','state','zipcode','latitude','longitude','property_type','tax_assessment','year_built','lot_size','sq_ft','bedrooms','bathrooms','estimated_mkt_value']
+        address_list = self.selectFile()
+         
+        # Check each record to see if Zillow can find it, add results to address_info dataframe
+        address_info = pandas.DataFrame(columns=column_list)
+        for row in address_list:
+            single_address = row[0][2:-1]+' '+row[1][2:-1]+' '+row[2][2:-1]
+            zipcode = row[-1][2:-1]
+             
+            # Excel removes leading 0, this adds them back
+            if len(str(zipcode)) < 5:
+                zipcode = str(zipcode).zfill(5)    
+             
+            get_address = pandas.DataFrame(self.address(single_address,zipcode),columns=column_list)
+            address_info = address_info.append(get_address)
+     
+        # Output address dataframe to .csv file
+        address_info.to_csv('address_test.csv',sep=',',index=False)
+    
+    def selectFile(self):
+        # Browse to directory of portfolio file
+        root2 = tkinter.Tk()
+        root2.withdraw()
+        currdir = os.getcwd()
+        validfile = False
+        while validfile == False: 
+            tempdir = tkFileDialog.askopenfilename(parent=root2, initialdir=currdir, title='Please select an address .csv file.')
+    
+            # Make sure file is correct type
+            if tempdir.endswith('.csv'):
+                portfolio_file = tempdir
+                address_list = np.loadtxt(portfolio_file,delimiter=',',skiprows=1,dtype=str)
+                
+                # Make sure file is in correct format with expected number of columns
+                if not address_list.shape[1] == 4:
+                    print ('Text file should have 4 columns: Street address, city, state, postal code. Please try again.')
+                else:
+                    validfile = True 
+            
+            # Handle cancel
+            elif len(tempdir) == 0:
+                validfile = True
+                sys.exit()
+            
+            # If file exists but doesn't end with .csv, is wrong file type
+            else:
+                print("Error: File type must be .csv.")      
+        return address_list         
+
+    
+    def address(self, single_address, zipcode):
+        # Call AddressData class methods
+        address_data = AddressData(single_address, zipcode)
+        results = address_data.get_deep_search_results()
+        
+        # Select desired property attributes from Zillow
+        attributes = ['result.address.street.contents',
+                      'result.address.city.contents',
+                      'result.address.state.contents',
+                      'result.address.zipcode.contents',
+                      'result.latitude.contents',
+                      'result.longitude.contents',
+                      'result.useCode.contents',
+                      'result.taxAssessment.contents',
+                      'result.yearBuilt.contents',
+                      'result.lotSizeSqFt.contents',
+                      'result.finishedSqFt.contents',
+                      'result.bedrooms.contents',
+                      'result.bathrooms.contents',
+                      'result.zestimate.amount.contents']
+        
+        def splitAttr(attrstring):
+            # Split attribute list, iteratively navigate XML tree
+            attrs = attrstring.split('.')
+            for i in range(len(attrs)):
+                if i == 0:
+                    lvl_1 = getattr(results, attrs[i])
+                else:
+                    lvl_2 = getattr(lvl_1, attrs[i]) 
+                    lvl_1 = lvl_2
+                    if i == len(attrs) - 1:
+                        lvl_1 = lvl_1[0]
+            return lvl_1        
+        
+        if results is not None:
+            dataset = [0] * len(attributes)
+            count = -1
+            
+            # Find results for all attributes, add to dataset list
+            for item in attributes:
+                count += 1
+                try:
+                    dataset[count] = splitAttr(item)
+                except (AttributeError, IndexError):
+                    continue            
+                
+            # Convert dataset to array, reshape output
+            dataset = np.array(dataset)
+            dataset = dataset.reshape((1,np.shape(dataset)[0]))
+            
+            return dataset
+        
+        else:
+            return
+        
+    
+
+
     
